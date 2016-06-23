36ea188f703bab7bcb5295e60541970a4d8c8098
laushinka <laurie.malau@gmail.com>
2016-06-22 21:55:36 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,22 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    # self because called on the instance
+    self.songs.first.genre
   end
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.count
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    count = 0
+    list_songs = self.songs
+    list_songs.each do |genre|
+      count += 1
+    end
+    count
   end
 end
